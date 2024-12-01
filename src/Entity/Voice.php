0b8fa687b5157e0a5bd794e03b578792bc3cde40
zuekveer <simetramonk@gmail.com>
2024-12-01 21:14:42 +0300

    upd Voice Entity

--- src/Entity/Voice.php
+++ src/Entity/Voice.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\CustomIdGenerator;
+use Doctrine\ORM\Mapping\GeneratedValue;
 
+#[ORM\Entity]
+#[ORM\HasLifecycleCallbacks]
 class Voice
 {
+    #[ORM\Id]
+    #[GeneratedValue(strategy: 'CUSTOM')]
+    #[CustomIdGenerator(class: UuidGenerator::class)]
+    #[Column(type: Types::GUID, unique: true, nullable: false)]
+    private string $id;
 
 }
