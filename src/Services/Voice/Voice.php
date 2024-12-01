e7496dd0639628936033d794c66537d53033589e
zuekveer <124701462+zuekveer@users.noreply.github.com>
2024-12-01 21:15:47 +0300

    DEV-2 : Artem : VoiceEntity (#7)
    * Voice ctrl, srv. fix
    
    * todo
    
    * upd Voice Entity

--- src/Services/Voice/Voice.php
+++ src/Services/Voice/Voice.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace App\Services\Voice;
 
+
 class Voice
 {
+    public function construct()
+    {
+
+    }
+
+    #TODO: add routes to get & post voices, only if role setted to admin
 
     #TODO: add routes to get & post voices, only if role setted to admin
 
